7082a073f4b3b92edc66b0fe84c44265678ba8b2
HW0001 <1600860624@qq.com>
2021-05-18 15:10:32 +0800

    init

---
+++ pages/home/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="content">
+		<KeyPanel></KeyPanel>
+	</view>
+</template>
+
+<script>
+	import KeyPanel from './keyPanel.vue'
+	export default {
+		data() {
+			return {}
+		},
+		components: {
+			KeyPanel
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		.a {
+			background-color: red;
+		}
+	}
+</style>
---
+++ pages/home/keyPanel.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="key-container">
+		<view v-for="(item,index) in keys" :key="index">
+			{{item}}
+		</view>
+	</view>
+</template>
+
+<script>
+	export default{
+		name:"KeyPanel",
+		data() {
+			return {
+				keys: [1,2,3,4,5,6,7,8,9]
+			}
+		},
+	}
+</script>
+
+<style>
+</style>
